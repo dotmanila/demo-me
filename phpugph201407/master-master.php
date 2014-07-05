0685f35a7ad98cf9f075307d2ab80263546260b0
Jervin R <social@dotmanila.com>
2014-07-04 21:22:33 -0400

    Fixed master-master

--- phpugph201407/master-master.php
+++ phpugph201407/master-master.php
@@ -1,54 +1,37 @@
 <?php
 define('HOST_PRIMARY', 'primary');
 $mysqli = connect_mysql();
+$host_info = false;
 
 $sql = "CREATE TABLE IF NOT EXISTS test(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY)";
 query_write_mysql($sql, $mysqli);
 
-for($i = 0; $i <= 1; $i++) {
+for($i = 0; $i <= 3; $i++) {
     $mysqli = connect_mysql();
 
     query_write_mysql("INSERT INTO test(id) VALUES (NULL)", $mysqli);
-
-    $ms = MYSQLND_MS_LAST_USED_SWITCH;
-    $sql = "/*ms=$ms*/SELECT MAX(id) AS id, @@server_id AS sid, connection_id() AS cid FROM test";
-    $res = query_select_mysql($sql, $mysqli);
-    if($res) {
-        $row = $res->fetch_assoc();
-        $res->close();
-        printf("Last value %04d from server id %d thread id %d\n", 
-            $row['id'], $row['sid'], $row['cid']);
-    }
-
+    printf("Last value %d from host %s and thread id %s\n", 
+        $mysqli->insert_id, $mysqli->host_info, $mysqli->thread_id);
     sleep(1);
+
+    $mysqli->close();
+    unset($mysqli);
 }
 
 function query_write_mysql($sql, $mysqli)
 {
+    global $host_info;
+    
     if ($mysqli->query($sql)) return true;
     /* We assign last errno to variable as somehow it is being cleared
     after first call */
     $errno = $mysqli->errno;
-    printf(__LINE__ . ": [%d] %s\n", $errno, $mysqli->error);
+    printf("ERRROR: %s [%d] %s on line %d\n", 
+        $host_info, $errno, $mysqli->error, __LINE__);
 
     if(is_connect_error($errno)) {
-        $mysqli = connect_mysql(true);
+        $mysqli = connect_mysql(true, $mysqli);
         return query_write_mysql($sql, $mysqli);
-    }
-    else return false;
-}
-
-function query_select_mysql($sql, $mysqli)
-{
-    if ($res = $mysqli->query($sql)) return $res;
-    /* We assign last errno to variable as somehow it is being cleared
-    after first call */
-    $errno = $mysqli->errno;
-    printf(__LINE__ . ": [%d] %s\n", $errno, $mysqli->error);
-
-    if(is_connect_error($errno)) {
-        $mysqli = connect_mysql(true);
-        return query_select_mysql($sql, $mysqli);
     }
     else return false;
 }
@@ -67,33 +50,36 @@ function is_connect_error($errno)
     return $is_connect_error;
 }
 
-function connect_mysql($retry = false)
+function connect_mysql($retry = false, $mysqli = false)
 {
+    global $host_info;
+
     $mysql_host = HOST_PRIMARY;
     $connected = true;
 
-    $mysqli = false;
-    $mysqli = mysqli_init();
-    $mysqli->real_connect($mysql_host, "revin", "revin", "test");
+    if(!$retry) {
+        $mysqli = false;
+        $mysqli = new mysqli($mysql_host, "revin", "revin", "test");
+        $host_info = $mysqli->host_info;
+        return $mysqli;
+    }
 
-    if(!$retry) return $mysqli;
-
-    $ms = MYSQLND_MS_LAST_USED_SWITCH;
     /* https://bugs.php.net/bug.php?id=67564 */
-    $mysqli->query("/*ms=$ms*/SELECT 1");
+    $mysqli->query("SELECT 1");
+    $host_info = $mysqli->host_info;
     if(!$mysqli->thread_id) $connected = false;
     $retries = 1;
 
     while(!$connected AND $retries <= 10) {
-        printf("Connection to host '{$mysql_host}' failed: [%d] %s, " . 
+        printf("Connection to host '{$host_info}' failed: [%d] %s, " . 
             "retrying ($retries of 10) in 3 seconds\n",
             $mysqli->connect_errno, $mysqli->error
         );
 
         sleep(3);
 
-        $mysqli->real_connect($mysql_host, "revin", "revin", "test");
-        $mysqli->query("/*ms=$ms*/SELECT 1");
+        $host_info = $mysqli->host_info;
+        $mysqli->query("SELECT 1");
 
         if(!$mysqli->thread_id) $connected = false;
         else {
